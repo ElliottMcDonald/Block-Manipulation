7c91b0fce0e38064b7dce746bf3e1675f6b039ff
ElliottMcDonald <e.mcdonald657@gmail.com>
2024-02-17 19:50:30 +0000

    Change x and y event listeners to prevent overflow Change event listener functions and x and y position sliders to include a calculation which considers width and object as a percentage of total container width, and to remove this amount from slider value if it would cause object overflow container and disappear off screen.

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,12 @@
+let object = document.querySelector(".object");
+
+let objectContainer = document.querySelector(".object-container")
+
 let output = document.querySelector(".output");
 
 let positionXSlider = document.querySelector("#position-x-slider");
+
+let positionYSlider = document.querySelector("#position-y-slider");
 
 output.textContent = positionXSlider.value;
 
@@ -10,15 +16,23 @@ positionXSlider.addEventListener("input", function () {
 
 // Manipulate Object on Y-Axis
 
-let positionYSlider = document.querySelector("#position-y-slider");
-
-let object = document.querySelector(".object");
-
 positionYSlider.addEventListener("input", function () {
-   object.style.marginTop = this.value + "px";
+   //Convert objects width to percentage of container's width
+   const objectHeightPercent = (object.clientHeight / objectContainer.clientHeight) * 100;
+   //Calculate max value of slider before object goes beyond container
+   const maxYSliderValue = 100 - objectHeightPercent;
+   //Take the slider's value unless it is greater than the maxSliderValue, if so 
+   //take that to stop object from going off screen.
+   let maxObjectMovement = Math.min(this.value, maxYSliderValue)
+   //Apply the chosen value as a marginTop style to the objectin percentage form.
+   object.style.marginTop = maxObjectMovement + "vh";
 });
 
 // Manipulate Object on X-Axis
+
 positionXSlider.addEventListener("input", function () {
-   object.style.marginLeft = this.value + "px";
+   const objectWidthPercent = (object.clientWidth / objectContainer.clientWidth) * 100;
+   const maxXSliderValue = 100 - objectWidthPercent;
+   let maxObjectMovement = Math.min(this.value, maxXSliderValue)
+   object.style.marginLeft = maxObjectMovement + "%";
 });
