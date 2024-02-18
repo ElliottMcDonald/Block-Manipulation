30f574f3cd5d5dfe01e6cb544bcb71a0427077c1
ElliottMcDonald <e.mcdonald657@gmail.com>
2024-02-18 15:19:36 +0000

    Simplify syntax of size slider. Add opacity slider

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,16 @@
-let object = document.querySelector(".object");
+const object = document.querySelector(".object");
 
-let objectContainer = document.querySelector(".object-container")
+const objectContainer = document.querySelector(".object-container");
 
-let output = document.querySelector(".output");
+const output = document.querySelector(".output");
 
-let positionXSlider = document.querySelector("#position-x-slider");
+const positionXSlider = document.querySelector("#position-x-slider");
 
-let positionYSlider = document.querySelector("#position-y-slider");
+const positionYSlider = document.querySelector("#position-y-slider");
 
-let sizeSlider = document.querySelector("#size-slider")
+const sizeSlider = document.querySelector("#size-slider");
+
+const opacitySlider = document.querySelector("#opacity-slider");
 
 output.textContent = positionXSlider.value;
 
@@ -20,12 +22,13 @@ positionXSlider.addEventListener("input", function () {
 
 positionYSlider.addEventListener("input", function () {
    //Convert objects width to percentage of container's width
-   const objectHeightPercent = (object.clientHeight / objectContainer.clientHeight) * 100;
+   const objectHeightPercent =
+      (object.clientHeight / objectContainer.clientHeight) * 100;
    //Calculate max value of slider before object goes beyond container
    const maxYSliderValue = 100 - objectHeightPercent;
-   //Take the slider's value unless it is greater than the maxSliderValue, if so 
+   //Take the slider's value unless it is greater than the maxSliderValue, if so
    //take that to stop object from going off screen.
-   let maxObjectMovement = Math.min(this.value, maxYSliderValue)
+   let maxObjectMovement = Math.min(this.value, maxYSliderValue);
    //Apply the chosen value as a marginTop style to the objectin percentage form.
    object.style.marginTop = maxObjectMovement + "vh";
 });
@@ -33,34 +36,26 @@ positionYSlider.addEventListener("input", function () {
 // Manipulate Object on X-Axis
 
 positionXSlider.addEventListener("input", function () {
-   const objectWidthPercent = (object.clientWidth / objectContainer.clientWidth) * 100;
+   const objectWidthPercent =
+      (object.clientWidth / objectContainer.clientWidth) * 100;
    const maxXSliderValue = 100 - objectWidthPercent;
-   let maxObjectMovement = Math.min(this.value, maxXSliderValue)
+   let maxObjectMovement = Math.min(this.value, maxXSliderValue);
    object.style.marginLeft = maxObjectMovement + "%";
 });
 
 //Manipulate Object Size
 
 sizeSlider.addEventListener("input", function () {
-   if (this.value == 1) {
-   object.style.width = 100 * (this.value) + "px";
-   object.style.height = 100 * (this.value) + "px";
-   }
-   else if (this.value == 1.5) {
-      object.style.width = 100 * (this.value) + "px";
-      object.style.height = 100 * (this.value) + "px"; 
-   }
-   else if (this.value == 2) {
-      object.style.width = 100 * (this.value) + "px";
-      object.style.height = 100 * (this.value) + "px";
-   }
+   scaleFactor = parseFloat(this.value);
+   //scaleFactor is * by 100 instead of object.clientHeight, as the latter
+   //will increase the height exponentially. Using * 100 resets it each time
+   //the slider moves.
+   object.style.width = scaleFactor * 100 + "px";
+   object.style.height = scaleFactor * 100 + "px";
 });
 
-// sizeSlider.addEventListener("input", function () {
-//    let scaleFactor = parseFloat(this.value); // Ensure the value is a number
-//    let newSize = 100 * scaleFactor; // Calculate new size based on the scale factor
+//Manipulate Object Opacity
 
-//    // Set the new size
-//    object.style.width = newSize + "px";
-//    object.style.height = newSize + "px"; // Assuming you want to scale both width and height equally
-// });
+opacitySlider.addEventListener("input", function () {
+   object.style.opacity = parseFloat(this.value);
+});
